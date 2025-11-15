7fdaee98cf32effb5d3a9022da7604a89a27326f
harinder.singh <harindersinghdeveloper@gmail.com>
2025-11-15 03:03:50 -0500

    variables & strings

---
+++ variables.py
@@ -0,0 +1,11 @@
+#all lower
+#do not start with number
+#do not start with special characters except "_"
+#global variables are CAPITAL
+
+number = 1
+print(type(number))
+number = "sam"
+print(type(number))
+number = 11.34
+print(type(number))
