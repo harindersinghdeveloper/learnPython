2049b76e3bb65b53d154c2ee0e5f639a40ce5195
Harinder Singh <harinder.singh@upshop.com>
2025-11-15 02:45:09 -0500

    first commit

---
+++ operator.py
@@ -0,0 +1,13 @@
+def multiply(a, b):
+    print(a*b)
+def divide(a, b):
+    print(a/b)
+def mod(a, b):
+    print(a%b)
+def power(a, b):
+    print(a**b)
+
+multiply(7, 4)
+divide(7,4)
+mod(7,4)
+power(7,4)
