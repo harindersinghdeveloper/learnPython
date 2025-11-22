bce6954fb926178490975de259f98c3e2da2faec
harinder.singh <harindersinghdeveloper@gmail.com>
2025-11-22 13:00:55 -0500

    files and control flows

---
+++ controlFlows.py
@@ -0,0 +1,19 @@
+a = 3
+b = 2
+
+if a < b:
+    print(a)
+else:
+    print(b)
+
+
+a=4
+
+if a==1:
+    print("a is 1")
+elif a==2:
+    print("a is 2")
+elif a==3:
+    print("a is 3")
+else:
+    print("a is greater than 3")
---
+++ loops.py
@@ -0,0 +1,7 @@
+mylist = [3,45,3,65,3,6,4,6,5,6,4,5,4,6,4,5,4,6,4,5,4,56,4,5,4,56,45]
+for item in mylist:
+    print(item)
+#only print even numbers
+for item in mylist:
+    if item % 2 == 0:
+        print(item)
