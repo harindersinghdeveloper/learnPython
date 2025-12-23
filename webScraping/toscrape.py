16886e0e4d6e216a7f4c6f7cefb7941a1e165e50
harinder.singh <harindersinghdeveloper@gmail.com>
2025-12-23 07:59:30 -0500

    webscraping

--- webScraping/toscrape.py
+++ webScraping/toscrape.py
@@ -1,3 +1,17 @@
 import requests
 import bs4
-
+import pdb
+#list 2-star books on all pages
+baseurl = "https://books.toscrape.com/catalogue/page-{}.html"
+for x in range(1,50):
+    page = requests.get(baseurl.format(x))
+    soup = bs4.BeautifulSoup(page.content, "lxml")
+    books = soup.select(".product_pod")
+    twoStarBooks = []
+    for book in books:
+        if book.find("p", class_='star-rating').get("class").__contains__("Two"):
+            #print(book)
+            h3 = book.find("h3")
+            title = h3.find("a").get("title")
+            twoStarBooks.append(title)
+    print(f'Two star books on page {x} are: {twoStarBooks}')
